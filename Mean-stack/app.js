edabf1141dc2d6afbb9aac79541f64cebb9c6c33
Pedro Fernandes <pedro92.gf@gmail.com>
2017-03-20 22:07:52 -0400

    Mean stack part 9.

---
+++ Mean-stack/app.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const config = require('./config/database')
+
+//Connect to database
+mongoose.connect(config.database);
+
+//On Connection
+mongoose.connection.on('connected', () => {
+	console.log('Connected to database ' + config.database);
+}); 
+
+//On Error
+mongoose.connection.on('error', (err) => {
+	console.log('Database error: ' + err);
+}); 
+
+//Express Initialization
+const app = express();
+
+const port = 3000;
+
+//CORS Middleware to accept connection between different ports (Frontend port - Backend port)
+app.use(cors());
+
+//Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Body Parser Middleware
+app.use(bodyParser.json());
+
+
+//Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport);
+
+
+const users = require('./routes/users');
+app.use('/users', users);
+
+
+//Index Route
+app.get('/', (req, res) => {
+	res.send('Invalid Endpoint');
+});
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+//Start Server
+app.listen(port, () => {
+	console.log('Server started on port ' + port);
+});
