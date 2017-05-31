9f1a67bceee8f9ece707d0cd51f8000fcab980e5
Pedro Fernandes <pedro92.gf@gmail.com>
2017-05-30 23:29:54 -0400

    nodekb register, login, passport.

--- nodekb/app.js
+++ nodekb/app.js
@@ -2,8 +2,13 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
+const config = require('./config/database');
 
-mongoose.connect('mongodb://localhost/nodekb');
+mongoose.connect(config.database);
 let db = mongoose.connection;
 
 //Check connection
@@ -33,6 +38,50 @@ app.use(bodyParser.json());
 //Set Public Folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Express Session
+app.use(session({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true
+}));
+
+//Express Messages Middleware
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+//Express Validator Middleware
+// In this example, the formParam value is going to get morphed into form body format useful for printing.
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+//Passport config
+require('./config/passport')(passport);
+//Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('*', (req, res, next) => {
+  res.locals.user = req.user || null;
+  next();
+});
+
 //Home route
 app.get('/', (req, res) => {
   Article.find({}, (err, articles) => {
@@ -47,82 +96,12 @@ app.get('/', (req, res) => {
   });
 });
 
-//Get Single Article
-app.get('/article/:id', (req, res) => {
-  Article.findById(req.params.id, (err, article) => {
-    res.render('article', {
-      article: article
-    });
-  });
-});
 
-
-
-//Add route
-app.get('/articles/add', (req, res) => {
-  res.render('add_article', {
-    title: "Add Article"
-  });
-});
-
-//Add submit POST Route
-app.post('/articles/add', (req, res) => {
-  let article = new Article();
-  article.title = req.body.title;
-  article.author = req.body.author;
-  article.body = req.body.body;
-
-  article.save((err) => {
-    if (err) {
-      console.log(err);
-      return;
-    } else {
-      res.redirect('/');
-    }
-  });
-});
-
-//Get Single Article
-app.get('/article/edit/:id', (req, res) => {
-  Article.findById(req.params.id, (err, article) => {
-    res.render('edit_article', {
-      title: 'Edit Article',
-      article: article
-    });
-  });
-});
-
-//Update submit POST Route
-app.post('/articles/edit/:id', (req, res) => {
-  let article = {};
-  article.title = req.body.title;
-  article.author = req.body.author;
-  article.body = req.body.body;
-
-  let query = {_id: req.params.id}
-
-  Article.update(query, article, (err) => {
-    if (err) {
-      console.log(err);
-      return;
-    } else {
-      res.redirect('/');
-    }
-  });
-});
-
-app.delete('/article/:id', (req, res) => {
-  let query = {_id: req.params.id}
-
-  Article.remove(query, (err) => {
-    if (err) {
-      console.log(err);
-    }
-
-    res.send('Success');
-
-  });
-});
+//Route files
+let articles = require('./routes/articles');
+let users = require('./routes/users');
+app.use('/articles', articles);
+app.use('/users', users);
 
 //Start server
 app.listen('3000', () => {
